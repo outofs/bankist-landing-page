8b579f865d09230e76c73e3ed15a1b9b5431313a
outofs <rtranscendental@gmail.com>
2023-02-05 22:04:36 +0200

    responsive design

--- script.js
+++ script.js
@@ -39,26 +39,6 @@ document.addEventListener('keydown', function (e) {
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {
-  e.preventDefault();
-  //   // const s1coords = section1.getBoundingClientRect();
-  //   // console.log(s1coords);
-  //   // console.log(e.target.getBoundingClientRect());
-  //   // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
-
-  //   // Scrolling
-  //   // window.scrollTo(
-  //   //   s1coords.left + window.pageXOffset,
-  //   //   s1coords.top + window.pageYOffset
-  //   // );
-  //   // window.scrollTo({
-  //   //   left: s1coords.left + window.pageXOffset,
-  //   //   top: s1coords.top + window.pageYOffset,
-  //   //   behavior: 'smooth',
-  //   // });
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
 /// Cookie message
 const header = document.querySelector('.header');
 const message = document.createElement('div');
